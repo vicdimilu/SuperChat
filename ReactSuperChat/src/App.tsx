5b1e2ddabf2b7e8af8a76498c0a4207791708482
Diego <vicdimilu@gmail.com>
2021-06-28 11:42:13 -0400

    Update SuperChat test client for server

--- ReactSuperChat/src/App.tsx
+++ ReactSuperChat/src/App.tsx
@@ -1,64 +1,53 @@
 import * as React from "react";
 import { extendTheme, Heading, ChakraProvider, Flex } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Chat } from "./components/chat/Chat";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { ChatMenu } from "./components/ChatMenu";
 import "emoji-mart/css/emoji-mart.css";
-
-/*SEND MESSAGES
-   socket.emit('chat message', "hola");
-
-   CONNECTION
-   const socket = io("http://127.0.0.1:5000");
-   READ MESSAGES
-   let messages : Array <string> = [];
-   socket.on("chat message", (msg : any) => {
-   messages.push(msg);
-   console.log(msg);
-   let showMsg = messages.map( msg => { <li>{msg}</li> })
-   })
- */
-
-export class App extends React.Component<any, any> {
-  render() {
-    return (
-      <ChakraProvider
-        theme={extendTheme({
-          fonts: {
-            heading: "Fredoka One",
-            body: "Raleway",
-            flex: "Raleway",
-            button: "Indie Flower",
-          },
-          styles: {
-            global: (props: any) => ({
-              body: {
-                bg: mode("gray.100", "#333")(props),
-              },
-            }),
-          },
-        })}
-      >
-        <Helmet title="Super Chat" />
-        <Flex position="absolute" top="1" right="1">
-          <ColorModeSwitcher m="0" p="0" />
-        </Flex>
-        <Flex align="center" direction="column" justify="center" h="100vh">
-          <Heading
-            position="fixed"
-            top="0"
-            w="100%"
-            m="2"
-            textAlign="center"
-            fontFamily="Fredoka One"
-            as="h1"
-          >
-            SuperChat
-          </Heading>
-          <Chat />
-        </Flex>
-      </ChakraProvider>
-    );
-  }
+type AppProps = {
+  children: any;
+};
+export enum EScreenOrientation {
+  LANDSCAPE = 1,
+  PORTRAIT = 2,
 }
+export const getScreenOrientation: () => EScreenOrientation = () =>
+  window.innerWidth < window.innerHeight
+    ? EScreenOrientation.PORTRAIT
+    : EScreenOrientation.LANDSCAPE;
+export const App = ({ children = null }: AppProps) => {
+  return (
+    <ChakraProvider
+      theme={extendTheme({
+        fonts: {
+          heading: "Fredoka One",
+          body: "Raleway",
+          flex: "Raleway",
+          button: "Indie Flower",
+        },
+        styles: {
+          global: (props: any) => ({
+            body: {
+              bg: mode("#333", "#333")(props),
+            },
+          }),
+        },
+      })}
+    >
+      <HelmetProvider>
+        <Helmet title="Super Chat" />
+        <Flex
+          w="100%"
+          align="center"
+          direction="column"
+          justify="center"
+          h="100vh"
+        >
+          <ChatMenu />
+          <Chat orientation={getScreenOrientation()} />
+        </Flex>
+      </HelmetProvider>
+    </ChakraProvider>
+  );
+};
--- ReactSuperChat/src/components/chat/Chat.tsx
+++ ReactSuperChat/src/components/chat/Chat.tsx
@@ -1,191 +1,63 @@
 import * as React from "react";
-//import { Textarea, Flex, Container, Heading, ChakraProvider, Box, Text, Link, VStack, Stack, Code, Grid, theme, Input, FormControl, FormLabel, FormErrorMessage, FormHelperText, Button, ButtonGroup} from "@chakra-ui/react";
-import {
-  Flex,
-  InputRightElement,
-  InputGroup,
-  Stack,
-  Input,
-  Button,
-} from "@chakra-ui/react";
+import { Flex, Stack } from "@chakra-ui/react";
+
 import { io } from "socket.io-client";
 import { ChatLibrary } from "./State.Interface";
-import { AnonymousForm } from "../forms/AnonymousForm";
-import { MessageList } from "./MessageList";
-import { Picker, Emoji } from "emoji-mart";
-import { FaSmileWink } from "react-icons/fa";
+import { ChatMessages } from "./MessageList";
+import { EScreenOrientation } from "../../App";
+import { ChatControls } from "../chat/ChatControls";
 
-export class Chat extends React.Component<any, any> {
-  messages: any = [];
-  nick: string = "Ex: Lucas";
-  url: string = "http://127.0.0.1:5000";
-  socket: any = io(this.url);
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      value: "Joining",
-      msgCount: 0,
-      userSocket: null,
-      pickerVisibility: "none",
-    };
-    //Init Functions
-    this.handleExitChat = this.handleExitChat.bind(this);
-    this.handleSendMessage = this.handleSendMessage.bind(this);
-    this.addEmoji = this.addEmoji.bind(this);
-    this.showEmojiPicker = this.showEmojiPicker.bind(this);
-  }
-
-  componentDidMount() {
+interface ChatProps {
+  orientation: EScreenOrientation;
+}
+export const Chat = ({ orientation }: ChatProps) => {
+  let url: string = "http://127.0.0.1:5000";
+  let socket: any = io(url);
+  const [messages, setMessages] = React.useState<any[]>([]);
+  const [userName, setUserName] = React.useState("AnonymousUser");
+  React.useEffect(() => {
+    socket.emit(ChatLibrary.Anonymous, userName);
     //activate messages receptor
-    this.socket.on(ChatLibrary.GeneralChatMessage, (message: string) => {
-      this.messages.push(message);
-      // We use an empty etState to rerender the component
-      this.setState({});
+    socket.on(ChatLibrary.GeneralChatMessage, (message: string) => {
+      setMessages(messages.concat(message));
     });
     //Login Receptor
-    this.socket.on(ChatLibrary.Anonymous, (packet: boolean) => {
-      if (packet) {
-        this.setState({ value: "Joined" });
-      }
-    });
+    socket.on(ChatLibrary.Anonymous, (_packet: boolean) => {});
     //Private Messeges Receptor
-    this.socket.on(ChatLibrary.PrivateChatMessage, (msg: any) => {
-      console.log("Chat: <componentDidMount> ", msg);
-    });
-  }
+    socket.on(ChatLibrary.PrivateChatMessage, (_msg: any) => {});
+    return () => socket.disconnect();
+  });
 
-  componentWillUnmount() {
-    this.socket.disconnect();
-  }
-
-  handleExitChat() {
-    this.updateState({ value: "Joining" });
-    this.socket.disconnect();
-  }
-
-  //Send Message METHOD
-  handleSendMessage(event: any) {
+  function handleSendMessage(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const MSGForm: HTMLFormElement = event.target;
+    const MSGForm: HTMLFormElement = event.currentTarget;
+    const MSG: string = MSGForm.MSGInput.value;
     //capturamos mensaje a enviar
-    this.socket.emit(ChatLibrary.GeneralChatMessage, MSGForm.MSGInput.value);
+    socket.emit(ChatLibrary.GeneralChatMessage, MSG);
     MSGForm.MSGInput.value = "";
   }
-
-  updateState(value: any) {
-    this.setState(value);
-    console.log("UPDATE STATE: ", this.state);
-  }
-
-  addEmoji(emojiObj: any) {}
-  showEmojiPicker() {
-    const visibilityValue: string =
-      this.state.pickerVisibility === "none" ? "auto" : "none";
-    this.setState({
-      pickerVisibility: visibilityValue,
-    });
-    return undefined;
-  }
-  render() {
-    if (this.state.value !== "Joining") {
-      return (
-        // This should be an "EntryForm" that ask for either
-        // a nickname (as an anonymous user) or login credentials (registered user)
-        <AnonymousForm userSocket={this.socket} />
-      );
-    } else {
-      return (
-        <Flex
-          w="75%"
-          h="50%"
-          justify="center"
-          align="center"
-          bgColor="white"
-          boxShadow="md"
-          rounded="lg"
-        >
-          <Stack
-            h="100%"
-            w="100%"
-            direction="column"
-            justify="space-between"
-            spacing={0}
-          >
-            <MessageList messages={this.messages} />
-            <Flex h="10%" align="center">
-              <form
-                style={{ width: "100%", padding: "2%" }}
-                onSubmit={this.handleSendMessage}
-              >
-                <Flex w="100%" align="center">
-                  <InputGroup size="md">
-                    <Input
-                      variant="flushed"
-                      name="MSGInput"
-                      id="MSGInput"
-                      type="text"
-                      size="xs"
-                      placeholder="Type here to message others."
-                      mr="1"
-                    />
-                    <InputRightElement position="relative" width="4.5rem">
-                      <Flex w="80%" justify="space-between">
-                        <Flex
-                          borderColor="blue"
-                          display={this.state.pickerVisibility}
-                          bottom="100"
-                          position="absolute"
-                          as="span"
-                        >
-                          <Picker
-                            set="google"
-                            title=""
-                            emoji=""
-                            showSkinTones={false}
-                            showPreview={false}
-                            onClick={undefined}
-                          />
-                        </Flex>
-                        <Button
-                          h="1.75rem"
-                          color="black"
-                          size="sm"
-                          onClick={this.showEmojiPicker}
-                        >
-                          <FaSmileWink />
-                        </Button>
-                      </Flex>
-                    </InputRightElement>
-                  </InputGroup>
-                  <Flex w="20%" justify="space-between">
-                    <Button
-                      variant="outline"
-                      fontFamily="Indie Flower"
-                      boxShadow="md"
-                      rounded="md"
-                      type="submit"
-                      size="xs"
-                    >
-                      Send
-                    </Button>
-                    <Button
-                      size="xs"
-                      fontFamily="Indie Flower"
-                      variant="outline"
-                      type="submit"
-                      rounded="md"
-                      onClick={this.handleExitChat}
-                      boxShadow="md"
-                    >
-                      Exit Chat
-                    </Button>
-                  </Flex>
-                </Flex>
-              </form>
-            </Flex>
-          </Stack>
-        </Flex>
-      );
-    }
-  }
-}
+  return (
+    <Flex
+      w="95%"
+      h="94.5%"
+      justify="center"
+      align="center"
+      bgColor="white"
+      boxShadow="md"
+      rounded="lg"
+      color="white"
+      mb="2.5vw"
+    >
+      <Stack
+        h="100%"
+        w="100%"
+        direction="column"
+        justify="space-between"
+        spacing={0}
+      >
+        <ChatMessages messages={messages} />
+        <ChatControls handleSendMessage={handleSendMessage} />
+      </Stack>
+    </Flex>
+  );
+};
